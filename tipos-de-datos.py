53a786b6f584436998bcb56e7cc94e1fb473c5b8
josemb176 <josemb176@gmail.com>
2024-09-13 18:37:55 -0500

    Se inicializa el repositorio bootcamp de inteligencia artificial

---
+++ tipos-de-datos.py
@@ -0,0 +1,20 @@
+"string" # string se utiliza para tipos de datos texto
+'string' # así también podemos escribir un string
+"""string""" # también podemos escribir asi un string
+'''string''' # por ejemplo así también
+
+"""Tus datos son
+    Nombre: Mauricio
+    Apellido: Burbano"""
+'''Tus datos son
+    Nombre: Jose
+    Apellido: Meneses'''
+    
+# En python tenemos diferentes tipos de números:
+4 # Entero o tipo de dato entero
+40.2 # Flotante o tipo de dato float
+
+# También tenemos el tipo de dato simple (bool) True o False
+
+False # False o tipo de dato bool, en python lo escribimos con la inicial en mayúscula
+True # True o tipo de dato bool, en python lo escribimos con la inicial en mayúscula
